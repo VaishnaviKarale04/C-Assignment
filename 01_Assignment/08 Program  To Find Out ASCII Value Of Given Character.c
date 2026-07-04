26d17da62d63c6f2e332728c89c140e2aa2403aa
Vaishnavi karale <karalepatilvaishnavi@gmail.com>
2026-07-04 19:22:04 +0530

    Create 08 Program To Find Out ASCII Value Of Given Character.c

---
+++ 01_Assignment/08 Program  To Find Out ASCII Value Of Given Character.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+    {
+        char ch = '\0';
+
+        printf("\n Enter a Character : ");
+        ch = getche();
+
+        printf("\n ASCII Value Of Character %c is : %d",ch ,ch);
+
+        getch();
+        return 0;
+    }
