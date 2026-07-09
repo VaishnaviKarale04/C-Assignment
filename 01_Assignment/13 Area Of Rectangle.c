905403d45b3a6dca7e53fdd96a51577fc3061dff
Vaishnavi karale <karalepatilvaishnavi@gmail.com>
2026-07-09 19:15:13 +0530

    13 Area Of Rectangle.c

---
+++ 01_Assignment/13 Area Of Rectangle.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+    {
+        float Width = 0, Height = 0, Area = 0;
+
+        printf("\n Enter a Width Of Rectangle :");
+        scanf("%f",&Width);
+
+        printf("\n Enter a Height Of Rectangle :");
+        scanf("%f",&Height);
+
+        Area = Width * Height;
+
+        printf("\n Area Of Rectangle : %f",Area);
+
+        getch();
+        return 0;
+    }
