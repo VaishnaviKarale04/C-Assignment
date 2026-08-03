0f20c2c6e4799d03e8f7c8c5eacf385e397742f3
Vaishnavi karale <karalepatilvaishnavi@gmail.com>
2026-08-03 20:41:25 +0530

    01 Program That Uses A Loop To Print The Numbers From 101 To 110 .c

---
+++ 03_Assignment/01 Program That Uses A Loop To Print The Numbers From 101 To 110 .c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+    {
+        int Num = 101;
+
+        printf("\n The Numbers From 101 To 110 => \n");
+
+        while(Num <= 110)
+        {
+            printf("\n %d ",Num);
+            Num++;
+        }
+
+        getch();
+        return 0;
+    }
